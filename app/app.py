42d1cc141d025c20e444d4ab2c7406ad110bf6a2
charanpobbathi <tejacharan284@gmail.com>
2026-02-21 16:39:17 +0530

    Pushing code to test [2]

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>Deployment Successful!</h1><p>Running on Amazon ECS Fargate.</p>"
+    return "<h1>Deployment Successful!</h1><p>Running on Amazon ECS Fargate .</p>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
